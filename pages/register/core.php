79f35a6d864aa58fd42a6b62506c91e624c2e8b0
Thum Choon Tat <thumchoontat@gmail.com>
2017-09-21 17:46:00 +0800

    uncomment clear func

--- pages/register/core.php
+++ pages/register/core.php
@@ -9,7 +9,7 @@
 	if ($user->valid()){
 		if ($user->registerUser()){
 			$message->set('Registration successful','success');
-			//$user->clear();
+			$user->clear();
 		}
 		else{
 			$message->set('Username "'.$user->getUsername().'" has been used.','warning');

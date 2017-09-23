0dfd56823bcc7896c750bec81827ec55a4c92779
Thum Choon Tat <thumchoontat@gmail.com>
2017-09-23 11:23:47 +0800

    change connection to West Europe database

--- model/class/dbconnector.php
+++ model/class/dbconnector.php
@@ -1,10 +1,17 @@
 <?php 
 	class DBConnector{
 		private static $instance = null;
+		/* SEA database
 		private $serverName = 'ap-cdbr-azure-southeast-b.cloudapp.net:3306';
 		private $username = 'b09993b1df234b';
 		private $password = '338cffee';
 		private $dbName = 'uia';
+		*/
+		
+		private $serverName = 'eu-cdbr-azure-west-b.cloudapp.net:3306';
+		private $username = 'bd2a2c14ef1eb4';
+		private $password = 'd225c350';
+		private $dbName = 'acsm_5271a3959550625';
 		private $conn = null;
 		
 		private function __construct(){
